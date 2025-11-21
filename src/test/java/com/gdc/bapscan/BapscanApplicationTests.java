0d47907a182e0d96dc75bb9d655e3ecb79f8d783
astrataraxia <astrataraxia@gmail.com>
2025-11-21 18:45:47 +0900

    Project initalize

---
+++ src/test/java/com/gdc/bapscan/BapscanApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gdc.bapscan;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BapscanApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
